03029332fafc28f7d4f9102d129e036b658a6316
xuqiangbao <591379342@qq.com>
2026-03-16 10:07:48 +0800

    新文章：blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.xuqiangbao.xyz",
+	site: "https://note.xuqiangbao.xyz",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
